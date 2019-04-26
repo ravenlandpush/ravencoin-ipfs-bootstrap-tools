d587659cac594c940a822d0fc980c95ed5815559
push <47545876+ravenlandpush@users.noreply.github.com>
2019-04-26 11:47:06 +0100

    Major update
    Added messages confirming pinning. 
    Added banner.
    Added progress bar.
    Added Exit traps for function to cleanly exit progressbar etc.

--- swarm-client.sh
+++ swarm-client.sh
@@ -8,11 +8,34 @@
 # to be added to cron.daily
 # Mirror all IPFS on Ravencoin chain from source at Seed master.
 
+spinner() {
+    local i sp n
+    sp='/-\|'
+    n=${#sp}
+    printf ' '
+    while sleep 0.1; do
+        printf "%s\b" "${sp:i++%n:1}"
+    done
+}
+
+finish() {
+killall swarm-client.sh
+}
+trap finish EXIT
+
 cat banner;
 sleep 3
 
 echo "Ravencoin IPFS Swarm + IPFS Mirror Service"; sleep 1;
 echo "created by push@ravenland.org (C) 2019"; sleep 1;
+
+echo "Connecting to master Swarm Server via IPFS"; sleep 1;
+# client command to connect to Ravenland Ravencoin swarm service
+ipfs swarm connect /ip4/91.134.145.4/tcp/4001/ipfs/QmdgR69oTuHDoUjRrhYSiz5cpkXCj371vmF6qx3uxPkSSJ
+
+echo "adding Ravenland Ravencoin IPFS Mirror Service to your IPFS Bootstrap configuration"; sleep 1;
+# client command to permanently bootstrap your client to Ravenland Ravencoin Swarm Service
+ipfs bootstrap add /ip4/91.134.145.4/tcp/4001/ipfs/QmdgR69oTuHDoUjRrhYSiz5cpkXCj371vmF6qx3uxPkSSJ
 
 
 curl -L bootstrap.ravenland.org/ravencoin_ipfs_list_$(date +%Y-%m-%d).txt > allipfs
@@ -22,14 +45,31 @@ mkdir sync
 
 cd sync;
 while read -r hash; do
-        wget "https://gateway.ravenland.org/ipfs/$hash";
+
+printf 'Synchronising Ravencoin IPFS_Hash Object $hash: '
+spinner &
+
+
+
+        wget -c "https://gateway.ravenland.org/ipfs/$hash";
+        kill "$!" 2> /dev/null;
+        wait "$!" 2> /dev/null;
+
+        printf "\nAdding Object as IPFS Pin..   "
+        spinner &
         ipfs pin add $hash;
+
+kill "$!" 2> /dev/null
+wait "$!" 2> /dev/null  # kill the spinner
+printf '\n';
+
 done < "../allipfs"
 
 
 #while read -r hash2; do
 #       ipfs pin add $hash2
 #done < "../allipfs"
+
 
 # client command to connect to Ravenland Ravencoin swarm service
 ipfs swarm connect /ip4/91.134.145.4/tcp/4001/ipfs/QmdgR69oTuHDoUjRrhYSiz5cpkXCj371vmF6qx3uxPkSSJ
