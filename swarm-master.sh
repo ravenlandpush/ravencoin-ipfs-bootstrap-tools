9d9e2e88c84b20571b6ae0d9fb5730a5f0966814
push <47545876+ravenlandpush@users.noreply.github.com>
2019-04-26 10:44:12 +0100

    Update swarm-master.sh
    remove unnecessary escaping

--- swarm-master.sh
+++ swarm-master.sh
@@ -6,4 +6,4 @@
 # There are better capbiltes coming to ipfs cluster, but they are not yet available. This seems best way to achieve
 # Ravencoin IPFS Master Mirror and swarm peer network.
 
-ipfs pin ls --type=recursive | awk '{print $1}' > "/path/to/server/public_html/ravencoin_ipfs_list_$(date +\"%Y-%m-%d\").txt"
+ipfs pin ls --type=recursive | awk '{print $1}' > /path/to/server/public_html/ravencoin_ipfs_list_$(date +%Y-%m-%d).txt
