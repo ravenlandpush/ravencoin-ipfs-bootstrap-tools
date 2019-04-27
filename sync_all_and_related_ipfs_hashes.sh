8edb8f4f233da2e3d8c8803a81856dadf48b27a3
push <47545876+ravenlandpush@users.noreply.github.com>
2019-04-27 23:45:52 +0100

    Ravencoin chain only, with validation
    Added support so that ipfs hashes are only downloaded by the client before adding,
    
    i.e. if the client node doesn't already have the object pinned download object and pin, if the client node does already have the object pinned, delete it from the sync dir used by the gateway. File isn't deleted after sync, but could be, if is_Valid is verified after each hash addition. 
    
    Leaving as is for now.
    
     Main goal is to prevents the clients downloading objects already pinned by swarm peers.

--- sync_all_and_related_ipfs_hashes.sh
+++ sync_all_and_related_ipfs_hashes.sh
@@ -6,5 +6,48 @@
 # cache. It may or may not be slower.
 
 # Use this script if you want the Ravencoin objects on IPFS but don't want to join the Ravenland Ravencoin swarm
+# A little bit less lightweight than desired.
 
-curl -L http://bootstrap.ravenland.org/ravencoin_ipfs_list_$(date +%Y-%m-%d).txt | xargs -i ipfs pin add -r {}
+# only works for dirs
+#curl -L http://bootstrap.ravenland.org/ravencoin_ipfs_list_$(date +%Y-%m-%d).txt | xargs -i ipfs pin add -r {}
+
+
+/home/ipfs/publish-ipfs-hashes-to-www.sh
+
+
+finish() {
+killall swarm-client-lightweight.sh
+}
+trap finish EXIT
+cd /home/ipfs/ravencoin-ipfs-bootstrap-tools
+cat banner;
+sleep 3
+
+echo "Ravencoin IPFS Swarm + IPFS Mirror Service"; sleep 1;
+echo "created by push@ravenland.org (C) 2019"; sleep 1;
+
+
+
+curl -L http://bootstrap.ravenland.org/ravencoin_chain_only_$(date +%Y-%m-%d).txt > allipfs
+mkdir sync;
+cd sync;
+while read -r hash; do
+
+
+#validate hash isnt present already before downloading
+is_valid=$(ipfs pin ls | grep $hash);
+echo "checking if $hash already present.."
+if [ -z "$is_valid" ]
+then
+# could use just curl -L and pipe to | ipfs add stdin
+wget -c https://gateway.ravenland.org/ipfs/$hash
+ipfs add $hash
+else
+echo "We have this file already pinned. Skipping..."
+#deletes file  on disk if already pinned, runs each start so eventually will empty
+# could be its own service ('clean ipfs sync dir when pinned found $is_valid')
+rm "/home/ipfs/sync/$hash"
+fi
+
+done < ../allipfs
+
