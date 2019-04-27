0f57088fca65d2b13044b3da6c52198f1b04bb5d
push <47545876+ravenlandpush@users.noreply.github.com>
2019-04-28 00:23:22 +0100

    Standardised script, added validation for Ravencoin hashes only
    Standard script. Downloads IPFS ravencoin has list for ravencoin hashes only on blockchain. Will check if the object is pinned before downloading it to increase efficiency and reduce bandwidth usage.

--- sync_all_not_related_ipfs_hashes.sh
+++ sync_all_not_related_ipfs_hashes.sh
@@ -4,5 +4,44 @@
 # to be revised with better automation soon
 
 #Sync all Ravencoin objects up to April 2019 for IPFS node;
-wget -c https://bootstrap.ravenland.org/ipfs.tar; tar -xvf ipfs.tar;cd ipfs; for i in $(ls); do ipfs add "$i"; done; 
+# deprecating
+#wget -c https://bootstrap.ravenland.org/ipfs.tar; tar -xvf ipfs.tar;cd ipfs; for i in $(ls); do ipfs add "$i"; done; 
+
+
+finish() {
+killall sync_all_not_related_ipfs_hashes.sh
+}
+trap finish EXIT
+cd /home/ipfs/ravencoin-ipfs-bootstrap-tools
+cat banner;
+sleep 3
+
+echo "Ravencoin IPFS Swarm + IPFS Mirror Service"; sleep 1;
+echo "created by push@ravenland.org (C) 2019"; sleep 1;
+
+
+
+curl -L http://bootstrap.ravenland.org/ravencoin_chain_only_$(date +%Y-%m-%d).txt > allipfs
+mkdir sync;
+cd sync;
+while read -r hash; do
+
+
+#validate hash isnt present already before downloading
+is_valid=$(ipfs pin ls | grep $hash);
+echo "checking if $hash already present.."
+if [ -z "$is_valid" ]
+then
+# could use just curl -L and pipe to | ipfs add stdin
+wget -c https://gateway.ravenland.org/ipfs/$hash
+ipfs add $hash
+else
+echo "We have this file already pinned. Skipping..."
+#deletes file  on disk if already pinned, runs each start so eventually will empty
+# could be its own service ('clean ipfs sync dir when pinned found $is_valid')
+rm "/home/ipfs/sync/$hash"
+fi
+
+done < ../allipfs
+
 
