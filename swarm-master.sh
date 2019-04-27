96630f672f5c16efbad5fa1d7aed184e76fd5d5e
push <47545876+ravenlandpush@users.noreply.github.com>
2019-04-28 00:25:49 +0100

    Added some detail about the difference between ravencoin only + related objects
    Shows the two methods (ravencoinonly commented out).  This will be standardized properly in the coming days/week.

--- swarm-master.sh
+++ swarm-master.sh
@@ -7,3 +7,10 @@
 # Ravencoin IPFS Master Mirror and swarm peer network.
 
 ipfs pin ls --type=recursive | awk '{print $1}' > /path/to/server/public_html/ravencoin_ipfs_list_$(date +%Y-%m-%d).txt
+
+
+# This won't make much sense to some, but this comes from our asset explorer backend and is how ravencoin_chain_only lists are generated
+# versus the list above which is generated on our Ravencoin Swarm Master Node and mirror service machine
+
+# ls -al /home/ipfs/ipfs-from-chain/ipfs | awk '{print $9'} > /path/to/www/public_html/ravencoin_chain_only_$(date +%Y-%m-%d).txt
+#chown -R nginx:nginx /path/to/www/public_html/
