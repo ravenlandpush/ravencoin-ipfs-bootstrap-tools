4e7f70fbdda223410e6a4ef3aaca6c252e9f0e7e
push <47545876+ravenlandpush@users.noreply.github.com>
2019-04-28 05:37:56 +0100

    Fix missing IPFS Hashes by hunting multiple gateways
    SImple but effective script at resuming bad, broken, missing or lost ipfs hash data. So that when the data becomes available on the network there is a maximum leverage on the DHT effort to provide the file, and client opportunity to obtian the file.

---
+++ fixmissingipfs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# A script that walks multiple gateways for an existing IPFS filesystem set of the IPFS_hash files
+# 0 byte files or missing files are resumed, 
+# using a loop like 
+# screen -S ipfshash-hunter while true; do ./fixmissingipfs.sh; done
+# allows you to run the script indefinitely. Better support coming this just a prototype. Will add support for more IPFS sources soon
+
+cd ipfs
+
+echo "Fix missing IPFS Hunter for Ravencoin by push@ravenland.org"; sleep 2;
+echo "Hunting for Lost or missing IPFS Hashes..."; sleep 1
+ls -altr | grep "root          0"
+
+
+ls -altr  | awk '{print $9}' | xargs -i wget -c -T 5 -t 2 https://gateway.ravenland.org/ipfs/{}
+ls -altr  | awk '{print $9}' | xargs -i wget -c -T 5 -t 2 https://cloudflare-ipfs.com/ipfs/{}
+ls -altr  | awk '{print $9}' | xargs -i wget -c -T 5 -t 2 https://ipfs.infura.io/ipfs/{}
+ls -altr  | awk '{print $9}' | xargs -i wget -c -T 5 -t 2 https://ipfs.io/ipfs/{}
